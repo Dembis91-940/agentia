2054f94f96b3cdc36eddfe537b0f2ea9700ba437
Dembis91-940 <agentiadeploiement@gmail.com>
2026-08-20 20:26:04 +0200

    FUTUR V3 : sync agentia (SEO + copy + design)

--- voice-config.js
+++ voice-config.js
@@ -1,1 +1,1 @@
-window.VOICE_CONFIG = {"publicKey": "2a56853c-e9e2-49b1-aaab-3f42eaf3312f", "assistantId": "2900041f-a7b8-4e93-b37d-c7e50fb06719", "accent": "#0d7a4b"};
+window.VOICE_CONFIG = {"publicKey": "2a56853c-e9e2-49b1-aaab-3f42eaf3312f", "assistantId": "2900041f-a7b8-4e93-b37d-c7e50fb06719", "accent": "#00ff88"};
